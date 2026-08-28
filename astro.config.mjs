cc1234f5c6bb47753ceaeafabeceaaf466546934
fuminaba <fumi73k@gmail.com>
2026-08-28 11:19:18 -0700

    Setting config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Replace with your GitHub username and repository name
+    site: 'https://nextpath-lab.github.io',
+});
